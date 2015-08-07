85f4992cd10d5d54ec8e75fd58754c88ac22989f
icepoint1 <351711778@qq.com>
2015-08-07 18:22:41 +0800

    a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,18 @@
 class MessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token ,
   def index
-@messages=Message.all
+@messages=Message.all.order(created_at: :DESC)
 
 
+
+  end
+  def create_image
+
+    post_image = PostImage.create(image: params[:upload],
+                                  user_id: current_user.id)
+    post_image.save
+
+    render text: "<script type=\"text/javascript\">window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{post_image.image.url}', '');</script>"
 
   end
 
